630242fdea42a3f4727516ccfc4488347d4051a5
Mick Niepoth <1678545+mickn@users.noreply.github.com>
2017-10-26 15:52:57 -0400

    Update

--- 00-markzones.py
+++ 00-markzones.py
@@ -8,7 +8,9 @@ def set_zones(who='*',sec=70):
     print("Press 'q' then ENTER in zone-setting window to skip to next video")
     print("Close zone-setting window to return to ipython prompt") 
     wait = raw_input("PRESS ENTER TO START SETTING ZONES")
-    vids = [f for f in glob(who+'.mp4') if not os.path.exists(viz_vidtools.epm_cfg_fn(f))]
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    vids = [f for f in glob(dir_path+'/'+who+'.mp4') if not os.path.exists(viz_vidtools.epm_cfg_fn(f))]
+    print vids
     vids = sorted(vids)
     for i,vid in enumerate(vids):
         print >> sys.stderr, 'Video %s of %s' % (i+1, len(vids))
@@ -19,4 +21,15 @@ def set_zones(who='*',sec=70):
             continue
         except ValueError:
             continue
-set_zones('20170530_113304_EPM_PO_11597_M',70) # Or set_zones('x'), where x can be *F2*, *F1*, *xf*, etc.   
+set_zones() # Or set_zones('x'), where x can be *F2*, *F1*, *xf*, etc.   
+
+print "Now, we'll set the points"
+
+from glob import glob
+import os
+from video_analysis import viz_vidtools
+vidsNoPointsSetYet = [f for f in glob('*.mp4') if not os.path.exists(viz_vidtools.epm_cfg_fn(f))]
+vidsOfInterest = [768]
+vids = [x for x in vidsNoPointsSetYet for y in vidsOfInterest if '_'+str(y)+'_' in x]
+for vid in vids:
+    viz_vidtools.get_epm_config(vid,70)
